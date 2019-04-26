1257a0cbf392445f13231a26a5d90992f36aa3ab
rlindst <45634974+rlindst@users.noreply.github.com>
2019-04-26 17:11:33 -0500

    update 2
    update 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let computerScore 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -15,12 +15,18 @@ function getComputerChoice() {
 	return choices [randomNUmber];
 }
 
+functon convertToWord(letter){
+	if (letter === "r") return "Rock";
+	if (letter === "p") return "Paper";
+	return "Scissors";
+}
+
 
 function win(userChoice, computerChoice) {
 	userScore++;
 	userScore_span.innerHTML = userScore;
 	computerScore_span.innerHTML = computerScore;
-	result_div.innerHTML = userChoice + "beats" + computerChoice + ". You Win!";
+	result_p.innerHTML = userChoice + "beats" + computerChoice + ". You Win!";
 }
 
 function lose() {
