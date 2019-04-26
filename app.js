5b9b5912412bb3afb9b442440d062c813732d985
rlindst <45634974+rlindst@users.noreply.github.com>
2019-04-26 17:02:14 -0500

    updates
    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore 0;
+let userScore = 0;
+let computerScore 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector(".score-board");
@@ -9,16 +9,63 @@ const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
 
-rock_div.addEventListener("click", function() {
-	console.log("hey you clicked on rock");
-})
-
-paper_div.addEventListener("click", function() {
-	console.log("hey you clicked on paper");
-})
-
-scissors_div.addEventListener("click", function() {
-	console.log("hey you clicked on scissors");
-})
+function getComputerChoice() {
+	const choices = ['r', 's', 'p'];
+	const randomNUmber = Math.floor(Math.random() * 3);
+	return choices [randomNUmber];
+}
 
 
+function win(userChoice, computerChoice) {
+	userScore++;
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_div.innerHTML = userChoice + "beats" + computerChoice + ". You Win!";
+}
+
+function lose() {
+	
+}
+
+function draw() {
+	console.log("DRAW");
+}
+
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice
+  switch(userChoice + computerChoice) {
+  	case "pr";
+  	case "sp";
+  	case "rs";
+  		win();
+  		break;
+  	case "rp";
+  	case "ps";
+  	case "sr";
+  		lose();
+  		break;
+  	case "rr"
+  	case "pp";
+  	case "ss";
+  		draw();
+
+  }
+}
+
+
+function main() {
+	rock_div.addEventListener('click', function() {
+	    game("r");
+
+
+	paper_div.addEventListener('click', function() {
+		game("p");
+	})
+
+	scissors_div.addEventListener('click', function() {
+		game("s");
+	})
+}
+
+main();
